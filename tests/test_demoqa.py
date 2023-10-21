06a810ea7c70d913fda33d4ffb02fd2c92fd7a3d
IT-Arkhipov <it.arkhipov@gmail.com>
2023-10-21 22:15:02 +0300

    form fill PageObject functions

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -3,29 +3,71 @@ from selene import browser, have, command
 from selenium.webdriver.common.keys import Keys
 
 
+class RegistrationForm:
+    def open(self):
+        browser.open("/automation-practice-form")
+
+    def fill_user_name(self, first_name: str, last_name: str):
+        browser.element('#firstName').type(first_name)
+        browser.element('#lastName').type(last_name)
+
+    def fill_email(self, email: str):
+        browser.element('#userEmail').type(email)
+
+    def select_gender(self, gender: str):
+        browser.element('#genterWrapper').all('label').element_by(have.text(gender)).click()
+
+    def fill_phone(self, number: str):
+        browser.element('#userNumber').type(number)
+
+    def fill_birthday(self, date: str):
+        browser.element('#dateOfBirthInput').send_keys(Keys.CONTROL + 'a').send_keys(date).press_enter()
+
+    def select_subject(self, subject: str):
+        browser.element('#subjectsInput').type(subject).press_enter()
+
+    def select_hobby(self, hobby: str):
+        browser.element('#hobbiesWrapper').perform(command.js.scroll_into_view)
+        browser.element('#hobbiesWrapper').all('label').element_by(have.text(hobby)).click()
+
+    def upload_picture(self, file_name: str):
+        browser.element('#uploadPicture').send_keys(os.path.abspath('img/' + file_name))
+
+    def fill_address(self, address: str):
+        browser.element('#currentAddress').type(address)
+
+    def select_state(self, state: str):
+        browser.element('#state').perform(command.js.scroll_into_view).click()
+        browser.element('#react-select-3-input').type(state).press_enter()
+
+    def select_city(self, city: str):
+        browser.element('#city').click()
+        browser.element('#react-select-4-input').type(city).press_enter()
+
+    def submit_form(self):
+        browser.element('footer').execute_script('element.remove()')
+        browser.element('#submit').perform(command.js.click)
+
+
 def test_demoqa_complete_form():
+    registration_form = RegistrationForm()
+
     # GIVEN
-    browser.open("/automation-practice-form")
+    registration_form.open()
 
     # WHEN
-    browser.element('#firstName').type('FirstName')
-    browser.element('#lastName').type('LastName')
-
-    browser.element('#userEmail').type('mymail@test.ru')
-    browser.element('#genterWrapper').all('label').element_by(have.text('Male')).click()
-    browser.element('#userNumber').type('9170770905')
-    browser.element('#dateOfBirthInput').send_keys(Keys.CONTROL + 'a').send_keys('11 Oct 2023').press_enter()
-    browser.element('#subjectsInput').type('Maths').press_enter()
-    browser.element('#hobbiesWrapper').perform(command.js.scroll_into_view)
-    browser.element('#hobbiesWrapper').all('label').element_by(have.text('Sports')).click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('img/sample.jpg'))
-    browser.element('#currentAddress').type('My current address')
-    browser.element('#state').perform(command.js.scroll_into_view).click()
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-    browser.element('#city').click()
-    browser.element('#react-select-4-input').type('Delhi').press_enter()
-    browser.element('footer').execute_script('element.remove()')
-    browser.element('#submit').perform(command.js.click)
+    registration_form.fill_user_name('FirstName', 'LastName')
+    registration_form.fill_email('mymail@test.ru')
+    registration_form.select_gender('Male')
+    registration_form.fill_phone('9170770905')
+    registration_form.fill_birthday('11 Oct 2023')
+    registration_form.select_subject('Maths')
+    registration_form.select_hobby('Sports')
+    registration_form.upload_picture('sample.jpg')
+    registration_form.fill_address('My current address')
+    registration_form.select_state('NCR')
+    registration_form.select_city('Delhi')
+    registration_form.submit_form()
 
     # THEN
     browser.element('.table-responsive').all('td:nth-of-type(2)').should(have.texts(
