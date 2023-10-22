7c575e2a1b4c837eaeb77ccf26c8a228cf6a5689
IT-Arkhipov <it.arkhipov@gmail.com>
2023-10-22 14:13:43 +0300

    dedicate parameters

---
+++ qa_guru_python_8_10/form/options.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class Gender(Enum):
+    male = 'Male'
+    female = 'Female'
+    other = 'Other'
+
+
+class Hobby(Enum):
+    sports = 'Sports'
+    reading = 'Reading'
+    music = 'Music'
